3ba6a6db1980f452c2e7a55d6ad44e126e48758c
CHANDAN KUMAAR <chandan7366881751@gmail.com>
2026-04-15 00:10:12 +0530

    Add a root route to return a Hello World message

--- Backend/app.js
+++ Backend/app.js
@@ -8,6 +8,10 @@ app.use(express.json());
 app.use("/api/v1/Admin", require("./routes/adminRoutes"));
 app.use("/api/v1/user", require("./routes/userRoutes"));
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 module.exports =app;
 
 
